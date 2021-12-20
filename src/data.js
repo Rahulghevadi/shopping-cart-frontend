f10cad709a677a3e12b46120e5aeaa550d90fef4
Rahul <rahulng503@gmail,com>
2021-12-20 20:32:18 +0530

    shoping-cart-ui

---
+++ src/data.js
@@ -0,0 +1,37 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name: 'Dell Laptop',
+            price: 45000,
+            image: 'https://images.unsplash.com/photo-1588872657578-7efd1f1555ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80',
+        },
+        {
+            id: '2',
+            name: 'Nike sheos',
+            price: 3000,
+            image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
+        },
+        {
+            id: '3',
+            name: 'HeadPhone',
+            price: 2000,
+            image: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
+        },
+        {
+            id: '4',
+            name: 'EarPhone',
+            price: 2000,
+            image: 'https://images.unsplash.com/photo-1583343894790-5cff219de28a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80',
+        },
+        {
+            id: '5',
+            name: 'OnoPlus 8',
+            price: 32000,
+            image: 'https://m.media-amazon.com/images/I/619iTNHSCGL._SL1500_.jpg',
+        }
+
+    ]
+};
+
+export default data;
